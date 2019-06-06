7b68b193645c8e3e92b028980db034d0faeb4728
Aluno <Aluno@cipucrs.pucrs.br>
2019-06-06 12:57:37 -0300

    Criado método que conta os números repetidos

--- src/pucrs/java/App.java
+++ src/pucrs/java/App.java
@@ -1,6 +1,5 @@
 package pucrs.java;
 
-import java.util.ArrayList;
 
 public class App {
 
@@ -9,20 +8,37 @@ public class App {
 		int vetor[] = new int[20];	
 		int vetorNormal[] = {2,5,7,1};
 		int vetorInvertido[] = new int[4];
+		int vetorOcorrencia[] = {1,2,3,4,1};
 		int aux = vetorNormal.length-1;
 		
-		System.out.println("Arranjo de 20 posições");
+		System.out.print("Arranjo de 20 posições: ");
 		for(int i = 0; i < vetor.length; i++) {
 			vetor[i] = i*10;
 			System.out.print(vetor[i] + " ");
 		}
 		
-		System.out.println("\n");
-		
+		System.out.println("");
+		System.out.print("Arranjo Invertido: ");
 		for(int i = 0; i < vetorNormal.length; i++) {
 			vetorInvertido[i] = vetorNormal[aux--];
 			System.out.print(vetorInvertido[i] + " ");
 		}
+		
+		System.out.println("");
+		
+		System.out.println("Numero Ocorrencias: " + nOcorrencias(vetorOcorrencia, 1));
+	}
+	
+	public static int nOcorrencias(int[] vetor, Integer el) {
+		int count = 0;
+		
+		for(int i = 0; i < vetor.length; i++) {
+			if(vetor[i] == el) {
+				count++;
+			}
+		}
+		
+		return count;
 	}
 
 }
