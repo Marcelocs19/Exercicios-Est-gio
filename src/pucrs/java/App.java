7d318aa9d5d0a102e36944c77e53e09835bb16d2
Marcelo Campos Silva <marcelo.home19@gmail.com>
2019-06-12 13:00:50 -0300

    Métodos nroRepeat e listRepeat consertados

--- src/pucrs/java/App.java
+++ src/pucrs/java/App.java
@@ -1,6 +1,7 @@
 package pucrs.java;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
@@ -11,7 +12,9 @@ public class App {
 		int vetorNormal[] = { 2, 5, 7, 1 };
 		int vetorInvertido[] = new int[4];
 		int vetorOcorrencia[] = { 1, 2, 3, 4, 1 };
-		int vetorRepetido[] = { 1, 2, 3, 4, 2};
+		int vetorRepetido[] = { 1, 2, 3, 4, 2 };
+		int vetorUnion[] = { 1, 2, 3 };
+		int vetorUnion2[] = { 4, 5 };
 		int aux = vetorNormal.length - 1;
 
 		System.out.print("Arranjo de 20 posições: ");
@@ -31,19 +34,25 @@ public class App {
 
 		System.out.println("Numero Ocorrencias: " + nOcorrencias(vetorOcorrencia, 1));
 
-		System.out.println("Numero Repetido: " + hasRepeat(vetorRepetido));
+		System.out.println("Tem Repetido: " + hasRepeat(vetorRepetido));
 
 		System.out.println("Numero Repetido: " + nroRepeat(vetorRepetido));
-		
+
 		System.out.print("Lista de valores repetidos: ");
 		int[] vetorAux = listRepeat(vetorRepetido);
-		
-		for(int i = 0; i < vetorAux.length; i++) {
+
+		for (int i = 0; i < vetorAux.length; i++) {
 			System.out.print(vetorAux[i] + " ");
+		}
+
+		System.out.println("\nUnião dois vetores: ");
+		int[] vetorAux2 = union(vetorUnion, vetorUnion2);
+		for (int i = 0; i < vetorAux2.length; i++) {
+			System.out.print(vetorAux2[i] + " ");
 		}
 	}
 
-	//a)
+	// a) Método que retorna quantas ocorrências de um elemento estão na lista
 	public static int nOcorrencias(int[] vetor, Integer el) {
 		int count = 0;
 
@@ -56,7 +65,7 @@ public class App {
 		return count;
 	}
 
-	//b)
+	// b) Método que retorna true se o vetor tem elementos repetidos
 	public static boolean hasRepeat(int[] vetor) {
 		int valor, valorRepetido;
 		boolean resposta = false;
@@ -75,74 +84,84 @@ public class App {
 		return resposta;
 	}
 
-	//c)
+	// c) Método que retorna o número de elementos repetidos
 	public static int nroRepeat(int[] vetor) {
-		int valor, valorRepetido;
-		int count = 0;
-		boolean aux = false;
-
-		for (int i = 0; i < vetor.length; i++) {
-			valor = vetor[i];
-			for (int j = 0; j < vetor.length; j++) {
-				valorRepetido = vetor[j];
-				if (i != j) {
-					if (valor == valorRepetido) {
-						aux = true;
-						count++;
-					}
+		int count = 0;	
+		ArrayList<Integer> lista = new ArrayList<>();
+		
+		for(int i = 0; i < vetor.length; i++) {
+			lista.add(vetor[i]);
+		}
+		
+		for (int i = 0; i < lista.size(); i++) {
+			for (int j = i+1; j < lista.size(); j++) {
+				if(lista.get(i) == lista.get(j)) {
+					lista.remove(j);
+					count++;
 				}
-			}
-			if (aux == true) {
-				count++;
-				break;
+				if(j > lista.size()) {
+					break;
+				}
 			}
 		}
 		return count;
 	}
-	
-	//d)
+
+	// d) Método que retorna um vetor dos elementos repetidos
 	public static int[] listRepeat(int[] vetor) {
-		int valor,valorRepetido;
-		List<Integer> lista = new ArrayList<Integer>();
+		List<Integer> listaVetor = new ArrayList<Integer>();
+		List<Integer> listaRepetidos = new ArrayList<Integer>();
 		int[] vetorRepetido;
 
 		for(int i = 0; i < vetor.length; i++) {
-			valor = vetor[i];
-			for (int j = 0; j < vetor.length; j++) {
-				valorRepetido = vetor[j];
-				if (i != j) {
-					if (valor == valorRepetido) {
-						lista.add(valorRepetido);
-					}
+			listaVetor.add(vetor[i]);
+		}
+		
+		for (int i = 0; i < listaVetor.size(); i++) {
+			for (int j = i+1; j < listaVetor.size(); j++) {
+				if(listaVetor.get(i) == listaVetor.get(j)) {
+					listaRepetidos.add(listaVetor.get(j));
+					listaVetor.remove(j);
+				}
+				if(j > listaVetor.size()) {
+					break;
 				}
 			}
 		}
-		vetorRepetido = new int[lista.size()];
-		for(int i = 0; i < lista.size(); i++) {
-			vetorRepetido[i] = lista.get(i);
+		vetorRepetido = new int[listaRepetidos.size()];
+		for(int i = 0; i < listaRepetidos.size(); i++) {
+			vetorRepetido[i] = listaRepetidos.get(i);
 		}
-		
 		return vetorRepetido;
 	}
-	
-	//e) Método que retorna a união dos vetores
+
+	// e) Método que retorna a união dos vetores
+	// Trabalhando
 	public static int[] union(int[] vetor1, int[] vetor2) {
-		int[] vetorUnion = new int[20];
-		
+		int[] vetorUnion = new int[vetor1.length + vetor2.length];
+		int i = 0;
+		for (; i < vetor1.length; i++) {
+			vetorUnion[i] = vetor1[i];
+		}
+
+		for (int j = 0; j < vetor2.length; j++) {
+			vetorUnion[i++] = vetor2[j];
+		}
+
 		return vetorUnion;
 	}
-	
-	//f) Metodo que retorna a intersecção dos vetores
+
+	// f) Metodo que retorna a intersecção dos vetores
 	public static int[] interserct(int[] vetor1, int[] vetor2) {
 		int[] vetorInterserct = new int[20];
-		
+
 		return vetorInterserct;
 	}
-	
-	//g) Método que retorna a diferença entre os vetores
+
+	// g) Método que retorna a diferença entre os vetores
 	public static int[] difference(int[] vetor1, int[] vetor2) {
 		int[] vetorDifference = new int[20];
-		
+
 		return vetorDifference;
 	}
 
